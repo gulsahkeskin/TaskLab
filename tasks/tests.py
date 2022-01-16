0f7ebf25a87e34b05590630f20cf33b5d9d937c4
Gulsah Keskin <keskingulsah92@gmail.com>
2022-01-16 16:31:08 +0300

    new tests added

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 
 from django.test import TestCase
+from django.urls import reverse
 
+from tasks.forms import TaskForm
 from tasks.models import Task
 
 
@@ -20,21 +22,43 @@ class UrlResponse(TestCase):
         self.assertEqual(response.status_code, 302)
 
 
-class TaskTestCase(TestCase):
+class TaskModelTest(TestCase):
+
+    def setUp(self):
+        # Create a user
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_user1.save()
+
+        # Create a task
+        task = Task.objects.create(title='some title', memo='explanations', progress=True)
+
+    def test_logged_in_uses_correct_template(self):
+        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        response = self.client.get(reverse('current_tasks'))
+
+        # Check our user is logged in
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        # Check that we got a response "success"
+        self.assertEqual(response.status_code, 200)
+
+        # Check we used correct template
+        self.assertTemplateUsed(response, 'tasks/current.html')
 
     @classmethod
     def setUpTestData(cls):
         # Set up non-modified objects used by all test methods
-        user = User.objects.create(username="gulsah", password="123")
-        Task.objects.create(user_id=1, title="title", memo="some text", progress=True)
+        Task.objects.create(title='docker', memo='compose')
 
-    # def setUp(self):
-    #     self.user = User.objects.create_user(username='testuser', password='12345')
-    #     Task.objects.create(title="title", memo="some text", progress=True, user=request.)
+    def test_task_created_successfully(self):
+        print("task named successfully")
+        task = Task.objects.get(id=1)
+        field_label = task._meta.get_field('title').verbose_name
+        field_memo = task._meta.get_field('memo').verbose_name
+        self.assertEqual(field_label, 'title')
+        self.assertEqual(field_memo, 'memo')
 
-    def test_task_is_created_successfully(self):
-        task = Task.objects.create(title="some title", memo="explanations", progress=True)
-        self.assertEqual(task.title, "some title")
-        self.assertEqual(task.user_id, 123)
-        self.assertEqual(task.memo, "explanations")
-        self.assertEqual(task.progress, True)
+    def test_create_new_task_form(self):
+        form_data = {'title': 'test task title', 'memo': 'test details', }
+        test_form = TaskForm(form_data)
+        self.assertTrue(test_form.is_valid())
+        test_form.save()
